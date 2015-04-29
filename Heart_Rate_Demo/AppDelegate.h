a5c39c5c361ea3bf489b9547146471cff02b15bd
TopsTech <tops1@Tops31.local>
2015-04-29 12:08:44 +0530

    Initial Commit

---
+++ Heart_Rate_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heart_Rate_Demo
+//
+//  Created by TopsTech on 29/04/2015.
+//  Copyright (c) 2015 tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
